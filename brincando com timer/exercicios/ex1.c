54b31ff6909f9af43e012ada5c4a432f6c0284f6
tui <70031851+tuisapo@users.noreply.github.com>
2022-08-17 10:34:50 -0300

    Create ex1.c

---
+++ brincando com timer/exercicios/ex1.c
@@ -0,0 +1,31 @@
+/*
+como o led deve ser ligado em low, ele deve ser polarizado reversamente. a malha é:
+3v3 -> resistor e led -> PC0 atuando como "gnd"
+
+	 *	0,2s = 1/1m6 * arr * psc
+	 *	3200000 = arr * psc
+	 *	psc = 1000
+	 *	arr = 3200
+	 */
+
+#include "stm32f4xx.h"
+
+int main(void)
+{
+	RCC->AHB1ENR=0x87;
+	GPIOC->MODER=0x01; // C0 como saida
+
+	RCC->APB2ENR|=0x40000;
+	TIM11->CR1=0x05;
+	TIM11->PSC=999;
+	TIM11->ARR=1599;
+
+	while (1) {
+
+		GPIOC->ODR^=0x01; // inverte PC0
+
+		while((TIM11->SR&0x01) == 0x00);
+		TIM11->SR&=~(0x01);
+
+	}
+}
