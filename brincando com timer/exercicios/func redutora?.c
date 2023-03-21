5c7d69b9909aba5b4eed04d8e281fb9bb6a9ffd1
tui <70031851+tuisapo@users.noreply.github.com>
2023-03-20 21:17:56 -0300

    Update func redutora?.c

--- brincando com timer/exercicios/func redutora?.c
+++ brincando com timer/exercicios/func redutora?.c
@@ -1,16 +1,18 @@
+// nao sei no q eu tava pensando mas existem no minimo uns 50 jeitos mais simples de fazer isso
+
 void t(int ms){
-	int arrQ = (ms*16) - 1;
-	while(arrQ > 65000){
+	int novoArr = (ms*16) - 1;
+	while(novoArr > 65000){
 		TIM11->ARR=65000;
 
 
 		while((TIM11->SR&0x01) == 0x00);
 		TIM11->SR&=~(0x01);
 
-		arrQ -= 65000;
+		novoArr -= 65000;
 	}
 
-	TIM11->ARR = arrQ;
+	TIM11->ARR = novoArr;
 
 	while((TIM11->SR&0x01) == 0x00);
 	TIM11->SR&=~(0x01);
